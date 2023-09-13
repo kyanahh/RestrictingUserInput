d0b0c23ef0577817d415a05b91b9a90454dfdb7e
kyanahh <kiannadominiquealvarez@gmail.com>
2023-09-13 16:01:39 +0800

    first commit

---
+++ GroupF2FLaboratory/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace GroupF2FLaboratory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSpecialCharactersonly = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtNoSpecialCharacters = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtNumbersonly = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtTextonly = new System.Windows.Forms.TextBox();
+            this.errorProviderApp = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderApp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(32, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RESTRICTING USER INPUT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(34, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Text only:";
+            // 
+            // txtSpecialCharactersonly
+            // 
+            this.txtSpecialCharactersonly.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtSpecialCharactersonly.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSpecialCharactersonly.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSpecialCharactersonly.ForeColor = System.Drawing.Color.White;
+            this.txtSpecialCharactersonly.Location = new System.Drawing.Point(269, 376);
+            this.txtSpecialCharactersonly.Name = "txtSpecialCharactersonly";
+            this.txtSpecialCharactersonly.Size = new System.Drawing.Size(215, 20);
+            this.txtSpecialCharactersonly.TabIndex = 5;
+            this.txtSpecialCharactersonly.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSpecialCharactersonly_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(34, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Numbers only:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(34, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "No special characters:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(34, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(194, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Special characters only:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(269, 395);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(215, 1);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.ForeColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(269, 325);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(215, 1);
+            this.panel2.TabIndex = 9;
+            // 
+            // txtNoSpecialCharacters
+            // 
+            this.txtNoSpecialCharacters.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtNoSpecialCharacters.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNoSpecialCharacters.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNoSpecialCharacters.ForeColor = System.Drawing.Color.White;
+            this.txtNoSpecialCharacters.Location = new System.Drawing.Point(269, 306);
+            this.txtNoSpecialCharacters.Name = "txtNoSpecialCharacters";
+            this.txtNoSpecialCharacters.Size = new System.Drawing.Size(215, 20);
+            this.txtNoSpecialCharacters.TabIndex = 4;
+            this.txtNoSpecialCharacters.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNoSpecialCharacters_KeyPress);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.ForeColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(269, 265);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(215, 1);
+            this.panel3.TabIndex = 11;
+            // 
+            // txtNumbersonly
+            // 
+            this.txtNumbersonly.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtNumbersonly.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNumbersonly.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumbersonly.ForeColor = System.Drawing.Color.White;
+            this.txtNumbersonly.Location = new System.Drawing.Point(269, 246);
+            this.txtNumbersonly.Name = "txtNumbersonly";
+            this.txtNumbersonly.Size = new System.Drawing.Size(215, 20);
+            this.txtNumbersonly.TabIndex = 3;
+            this.txtNumbersonly.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumbersonly_KeyPress);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.ForeColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(269, 201);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(215, 1);
+            this.panel4.TabIndex = 13;
+            // 
+            // txtTextonly
+            // 
+            this.txtTextonly.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtTextonly.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTextonly.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTextonly.ForeColor = System.Drawing.Color.White;
+            this.txtTextonly.Location = new System.Drawing.Point(269, 182);
+            this.txtTextonly.Name = "txtTextonly";
+            this.txtTextonly.Size = new System.Drawing.Size(215, 20);
+            this.txtTextonly.TabIndex = 2;
+            this.txtTextonly.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTextonly_KeyPress);
+            // 
+            // errorProviderApp
+            // 
+            this.errorProviderApp.ContainerControl = this;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.Color.White;
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCheck.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(269, 445);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(215, 42);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.ForeColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(269, 145);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(215, 1);
+            this.panel5.TabIndex = 17;
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.ForeColor = System.Drawing.Color.White;
+            this.txtName.Location = new System.Drawing.Point(269, 126);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(215, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            this.txtName.Validating += new System.ComponentModel.CancelEventHandler(this.txtName_Validating);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(34, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 21);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(522, 520);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.txtTextonly);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.txtNumbersonly);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtNoSpecialCharacters);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSpecialCharactersonly);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restricting User Input";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderApp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSpecialCharactersonly;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtNoSpecialCharacters;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtNumbersonly;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtTextonly;
+        private System.Windows.Forms.ErrorProvider errorProviderApp;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ GroupF2FLaboratory/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroupF2FLaboratory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if(ValidateChildren(ValidationConstraints.Enabled))
+            {
+                MessageBox.Show(txtName.Text, "Demo App - Message!");
+            }
+        }
+
+        private void txtName_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                e.Cancel = true;
+                txtName.Focus();
+                errorProviderApp.SetError(txtName, "Name should not be left blank!");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProviderApp.SetError(txtName, "");
+            }
+        }
+
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTextonly_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNumbersonly_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNoSpecialCharacters_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetterOrDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtSpecialCharactersonly_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
